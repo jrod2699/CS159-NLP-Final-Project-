f83fceed27bc1fcd2289c4fb052174544770ae8e
samesoul <joonsungso@gmail.com>
2020-11-13 16:12:32 -0500

    added comments describing our plan to read sentence pairs

--- IBMBase.py
+++ IBMBase.py
@@ -6,6 +6,9 @@ def preprocess(filename):
     f = open(filename, "r")
     for line in f:
         strs = line.split("\t")
+        # OUR PLAN: split string by tab, index into english sentence (first index), foreign sentence (2nd index)
+            # then, replace add a "space" before any punctuation (using regex).
+            # finally, split by whitespace. we now have resultant eng and foreign arrays
         eng_sent = []
         for_sent = strs[1]
          
