7e6b429bf2ee06b969f17689a25234472e5f129c
jrod2699 <44716586+jrod2699@users.noreply.github.com>
2020-11-22 21:23:56 -0800

    Did some cleaning and fixed minor bugs in runibm2.py file

--- code/eval.py
+++ code/eval.py
@@ -37,32 +37,6 @@ def eval(test_alignments):
         print("-" * 47, "\n")
     f.close()
 
-def get_alignments(filename):
-    f = open(filename, "r")
-
-    for line in f:
-        # lowercase all words in the line -- this includes eng + for sentence
-        line = line.lower()
-        strs = line.split("\t")
-
-        # OUR PLAN: split string by tab and index into resultant array to 
-        # access the english sentence (first index) and 
-        # foreign sentence (2nd index)
-        eng_text = strs[0]
-        for_text = strs[1]
-
-        # Tokenize on white space
-        tokenizer = TweetTokenizer()
-        eng_words = tokenizer.tokenize(eng_text)
-        for_words = tokenizer.tokenize(for_text)
-
-        # Create alignment pairs and add to corpus
-        # aligned_sentence = AlignedSent(for_words, eng_words)
-        
-        for a in corpus:
-            if(a.mots == eng_words and a.words == for_words):
-                print(" ".join(a.words), "\t", " ".join(a.mots), "\t", a.alignment)
-    f.close
 
 def main():
     eval("data/evaluation tests/random.spanish")
--- code/runibm2.py
+++ code/runibm2.py
@@ -1,7 +1,7 @@
 import nltk
 import string
 import random
-from IBMBase import preprocess
+from IBMBase import compile_corpus
 from nltk.translate import IBMModel2, AlignedSent, Alignment
 from nltk.metrics import precision, recall
 from nltk.tokenize import TweetTokenizer
@@ -16,54 +16,40 @@ def run(filename, iterations):
     corpus = compile_corpus(filename)
     ibm2 = IBMModel2(corpus, iterations)
 
-    # produce random sentences for testing purposes
-    get_rand_sent("data/rand_out.test")
+    # produce the alignments of the test sentences
+    get_rand_sent("data/test sentences/ibm2/french.ibm2")
 
-def get_rand_sent(newfile):
-    ''' 
-    Redirect the standard output of the program -- i.e. the random sentences --
-    and transfer it over to the appropriate file. From there we will take a 
-    look at the sentence pair and include the hand alignment (gold standard)
-    to proceed with evaluating the IBM model.
-    '''
-    i = 0
-    while i < 10:
-        index = random.randint(0, len(corpus))
-        try:
-            # only print out "valid" sentence pairs
-                # valid = sentence pairs with system-created alignments
-            print(" ".join(corpus[index].words), "\t", " ".join(corpus[index].mots), "\t", corpus[index].alignment)
-            i += 1
-        except:
-            pass
-
-
-def eval(test_alignments):
-    f = open(test_alignments, "r")
+def get_alignments(filename):
+    f = open(filename, "r")
 
     for line in f:
+        # lowercase all words in the line -- this includes eng + for sentence
+        line = line.lower()
         strs = line.split("\t")
 
-        sys_aligns = Alignment.fromstring(strs[2])
-        hand_aligns = Alignment.fromstring(strs[3])
+        # OUR PLAN: split string by tab and index into resultant array to 
+        # access the english sentence (first index) and 
+        # foreign sentence (2nd index)
+        eng_text = strs[0]
+        for_text = strs[1]
 
-        '''
-        Evaluate the sentence pair's precisiona and recall by utilizing the
-        built in ntlk.metrics precision and recall functions. The functions 
-        parameters are the following:
-            1. Reference ("Gold Standard"): our hand alignments that follow the same format
-            as the system produced alignments
-            2. Test: the alignments produced by the model which will be put in
-            comparison with the hand alignments 
-        '''
-        print("Precision: ", precision(hand_aligns, sys_aligns))
-        print("Recall: ", recall(hand_aligns, sys_aligns))
-    f.close()
+        # Tokenize on white space
+        tokenizer = TweetTokenizer()
+        eng_words = tokenizer.tokenize(eng_text)
+        for_words = tokenizer.tokenize(for_text)
+
+        # Create alignment pairs and add to corpus
+        # aligned_sentence = AlignedSent(for_words, eng_words)
+        
+        for a in corpus:
+            if(a.mots == eng_words and a.words == for_words):
+                print(" ".join(a.words), "\t", " ".join(a.mots), "\t", a.alignment)
+    f.close
 
 
 def main():
-    # run("data/languages/10000french.txt", 5)
-
+    run("data/languages/fra-eng.txt", 5)
+    
 
 if __name__ == "__main__":
     main()
